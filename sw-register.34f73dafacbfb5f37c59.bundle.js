08c7f7dcfa882d5f6e32415dd82314c4df8b4329
Ling Kai Zhu <pig.junior@gmail.com>
2017-05-08 21:41:00 -0400

    20170508

---
+++ sw-register.34f73dafacbfb5f37c59.bundle.js
@@ -0,0 +1,1 @@
+webpackJsonp([3,5],{"3WLI":function(r,e){"serviceWorker"in navigator&&navigator.serviceWorker.register("worker-basic.min.js").catch(function(r){console.error("Error registering service worker:",r)})},5:function(r,e,o){r.exports=o("3WLI")}},[5]);
